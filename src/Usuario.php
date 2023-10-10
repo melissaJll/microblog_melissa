b8d574fab69c307a1b77a05825fa2b2781ea82d0
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-10-10 08:56:13 -0300

    classe Usuario

---
+++ src/Usuario.php
@@ -0,0 +1,78 @@
+<?php
+namespace Microblog;
+use PDO, Exception;
+
+class Usuario{
+    private int $id;
+    private string $nome;
+    private string $email;
+    private string $senha;
+    private string $tipo;
+    private PDO $conexao;
+
+    public function __construct() {
+        $this->conexao = Banco::conecta();//método estatico
+    }
+
+
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+
+    }
+
+
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+    public function setNome(string $nome): void
+    {
+        $this->nome = $nome;
+    }
+
+
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+
+    
+    public function getSenha(): string
+    {
+        return $this->senha;
+    }
+    public function setSenha(string $senha): void
+    {
+        $this->senha = $senha;
+
+    }
+
+
+    
+    public function getTipo(): string
+    {
+        return $this->tipo;
+    }
+
+    public function setTipo(string $tipo): void
+    {
+        $this->tipo = $tipo;
+
+    }
+
+
+    
+}
