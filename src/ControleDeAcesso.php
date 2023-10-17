cd6e6291e944933d14cfad1a09630422e02d79cc
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-10-17 09:27:29 -0300

    método login com os dados

--- src/ControleDeAcesso.php
+++ src/ControleDeAcesso.php
@@ -19,6 +19,14 @@ final class ControleDeAcesso{
         }
     }
 
+    public function login(int $id, string $nome, string $tipo): void{
+        //SESSION Funciona como array associativo 
+        //No mommento que ocorre o login, criamos variáveis de sessão contetndo os dados que queremos monitorar/controlar através da sessão enquanto estiver logada
+        $_SESSION["id"] = $id;
+        $_SESSION["nome"] = $nome;
+        $_SESSION["tipo"] = $tipo;
+    }
+
 } 
 
 ?>
