686e637ff2463eff0e1c55c6b767f29eb6944da6
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-10-26 09:11:35 -0300

    método exluir noticia

--- src/Noticia.php
+++ src/Noticia.php
@@ -151,6 +151,35 @@ final class Noticia{
 
 
 
+    public function excluir():void {
+        if($this->usuario->getTipo() === "admin"){
+            $sql = "DELETE FROM noticias 
+                     WHERE id = :id";
+        } else {
+            $sql = "DELETE FROM noticias 
+            WHERE id = :id AND usuario_id = :usuario_id"; //editor
+        }
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            
+            if($this->usuario->getTipo() !== "admin"){
+                $consulta->bindValue(
+                    ":usuario_id", $this->usuario->getId(), PDO::PARAM_INT
+                );
+            }
+
+            $consulta->execute();
+        } catch (Exception $erro) {
+            die("Erro ao excluir notícia: " . $erro->getMessage());
+        }
+    }
+
+
+
+
+
     // Imagem retorna array com type[] name[] path[] ["tmp_name"]=>...
     public function upload(array $arquivo): void{
         //Definindo tipos válidos
