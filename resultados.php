a5025c33ab1f3e3df4610e7078fe2a3deafc9981
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-10-30 20:37:35 -0300

    Método busca

--- resultados.php
+++ resultados.php
@@ -1,42 +1,36 @@
-<?php 
+<?php
+
+use Microblog\Utilitarios;
+
 require_once "inc/cabecalho.php";
 $noticia->setTermo($_GET["busca"]);
+$resultados = $noticia->busca();
 ?>
-
 
 <div class="row bg-white rounded shadow my-1 py-4">
     <h2 class="col-12 fw-light">
         Você procurou por <span class="badge bg-dark"> <?=$noticia->getTermo()?> </span> e
-        obteve <span class="badge bg-info">X</span> resultados
+        obteve <span class="badge bg-info"> <?=count($resultados)?> </span> resultados
     </h2>
     
+    <?php foreach($resultados as $itemNoticia){ ?>
     <div class="col-12 my-1">
         <article class="card">
             <div class="card-body">
-                <h3 class="fs-4 card-title fw-light">Título da notícia...</h3>
+                <h3 class="fs-4 card-title fw-light">
+                    <?=$itemNoticia['titulo']?></h3>
                 <p class="card-text">
-                    <time>Data da notícia</time> - 
-                    Resumo da notícia.
+                    <time><?=Utilitarios::formataData($itemNoticia['data'])?></time> - 
+                    <?=$itemNoticia['resumo']?>
                 </p>
-                
-                <a href="noticia.php" class="btn btn-primary btn-sm">Continuar lendo</a>
+                <a href="noticia.php?id=<?=$itemNoticia['id']?>" 
+                class="btn btn-primary btn-sm">Continuar lendo</a>
             </div>
         </article>
     </div>
+    <?php } ?>
 
-    <div class="col-12 my-1">
-        <article class="card">
-            <div class="card-body">
-                <h3 class="fs-4 card-title fw-light">Título da notícia...</h3>
-                <p class="card-text">
-                    <time>Data da notícia</time> - 
-                    Resumo da notícia.
-                </p>
-                
-                <a href="noticia.php" class="btn btn-primary btn-sm">Continuar lendo</a>
-            </div>
-        </article>
-    </div>
+    
 
 </div>        
 
@@ -44,4 +38,3 @@ $noticia->setTermo($_GET["busca"]);
 require_once "inc/todas.php";
 require_once "inc/rodape.php";
 ?>
-
