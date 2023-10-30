f9cd9ead98fde1b1152f3b98d0686bf1fe08f6b0
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-10-30 20:48:27 -0300

    Ajustes HTML

--- inc/cabecalho.php
+++ inc/cabecalho.php
@@ -55,11 +55,15 @@ $listaCategorias = $noticia->categoria->listar();
         </li>
       </ul>
 
-      <form autocomplete="off" class="d-flex" action="resultados.php" method="GET">
-        <input name="busca" class="form-control me-2" type="search" placeholder="Pesquise aqui" aria-label="Pesquise aqui">
-        <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">OK</button>
-      </form>
-      
+      <div class="position-relative">
+        <form autocomplete="off" class="d-flex" action="resultados.php" method="POST" onsubmit="return false" id="form-busca">
+          <input id="campo-busca" name="busca" class="form-control me-2" type="search" placeholder="Pesquise aqui" aria-label="Pesquise aqui">  
+        </form>
+
+        <div id="resultados" 
+        class="mt-3 position-absolute container bg-white shadow-lg p-3 rounded"></div>
+      </div>
+
     </div>
   </div>
 </nav>
